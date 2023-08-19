392715ac0f0f06c24b206ffd8998322b4cda2eba
Roobsk91 <rubyasmith91@gmail.com>
2023-08-19 16:51:22 +0100

    Added code and portforlio links

--- src/App.js
+++ src/App.js
@@ -4,7 +4,13 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      Weather App
+      <h1>Weather App</h1>
+      <footer>
+        This project was created by <a href="https://elegant-daffodil-345aac.netlify.app/" target="_blank">Ruby Smith</a> and is {" "}
+        <a href="https://github.com/Roobsk91/react-final-project" target="_blank">
+        open-sourced on GitHub.
+      </a>
+      </footer>
     </div>
   );
 }
