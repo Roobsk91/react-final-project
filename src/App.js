d8fe4310015e1680fa01bc4a0297e6534a1b49d4
Roobsk91 <rubyasmith91@gmail.com>
2023-08-19 17:19:48 +0100

    Created basic format for HTML

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 
 import './App.css';
+import Weather from "./Weather";
 
 function App() {
   return (
     <div className="App">
       <div className="container">
-      <h1>Weather App</h1>
+      <Weather />
       <footer>
         This project was created by <a href="https://elegant-daffodil-345aac.netlify.app/" target="_blank">Ruby Smith</a> and is {" "}
         <a href="https://github.com/Roobsk91/react-final-project" target="_blank">
