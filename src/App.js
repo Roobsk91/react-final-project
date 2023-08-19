ea8e3d97f3ee086d22023898847898c500ac1826
Roobsk91 <rubyasmith91@gmail.com>
2023-08-19 20:49:16 +0100

    added rel-noreferrer

--- src/App.js
+++ src/App.js
@@ -8,8 +8,8 @@ function App() {
       <div className="container">
       <Weather defaultCity ="Seoul" />
       <footer>
-        This project was created by <a href="https://elegant-daffodil-345aac.netlify.app/" target="_blank">Ruby Smith</a> and is {" "}
-        <a href="https://github.com/Roobsk91/react-final-project" target="_blank">
+        This project was created by <a href="https://elegant-daffodil-345aac.netlify.app/" target="_blank" rel="noreferrer">Ruby Smith</a> and is {" "}
+        <a href="https://github.com/Roobsk91/react-final-project" target="_blank" rel="noreferrer">
         open-sourced on GitHub.
       </a>
       </footer>
